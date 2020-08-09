f33c10b9e9c6c58979e3419ad848f336b68c6482
abdelfatah <a.abdelfatah@itv-me.com>
2020-08-10 03:48:00 +0400

    testing routes with product

---
+++ routes/product.ts
@@ -0,0 +1,176 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+let products = [
+  {
+    id: "1",
+    title: "iphone 11",
+    price: 1000,
+    currency: "USD",
+  },
+  {
+    id: "2",
+    title: "iphone 12",
+    price: 2000,
+    currency: "USD",
+  },
+  {
+    id: "3",
+    title: "iphone 13",
+    price: 3000,
+    currency: "USD",
+  },
+];
+
+const router = new Router();
+//get all products
+router.get("/api/v1/products", ({ response }: { response: any }) => {
+  response.body = {
+    success: true,
+    data: products,
+  };
+});
+//get product details by id
+router.get(
+  "/api/v1/products/:id",
+  ({ params, response }: { params: { id: string }; response: any }) => {
+    let productId = params.id;
+    if (!productId) {
+      response.status = 400;
+      response.body = {
+        status: false,
+        msg: "Product Id is required",
+      };
+    }
+    let productDetails = products.find((product) => {
+      return product.id === productId;
+    });
+    if (productDetails) {
+      response.status = 200;
+      response.body = {
+        status: true,
+        data: productDetails,
+      };
+    } else {
+      response.status = 400;
+      response.body = {
+        status: false,
+        msg: "This product Does not exist",
+      };
+    }
+  }
+);
+router.post(
+  "/api/v1/products",
+  async ({ request, response }: { request: any; response: any }) => {
+    const body = await request.body();
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        status: false,
+        msg: "Missed Parameters",
+      };
+    }
+    let product = await body.value;
+    const errors = [];
+    if (!product.id) {
+      errors.push("id is required");
+    }
+    if (!product.title) {
+      errors.push("title is required");
+    }
+    if (!product.price) {
+      errors.push("price is required");
+    }
+    if (!product.currency) {
+      errors.push("currency is required");
+    }
+    if (errors.length !== 0) {
+      response.status = 400;
+      response.body = {
+        status: false,
+        errors: errors,
+      };
+    } else {
+      products.push(product);
+      response.status = 201;
+      response.body = {
+        status: true,
+        data: product,
+      };
+    }
+  }
+);
+router.put(
+  "/api/v1/products/:id",
+  async ({
+    params,
+    request,
+    response,
+  }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  }) => {
+    let body = await request.body();
+    let productId = params.id;
+    if (!productId) {
+      (response.status = 400),
+        (response.body = {
+          status: false,
+          msg: "ProductId is required",
+        });
+    }
+    //get product
+    let productDetails = products.find((product) => {
+      return product.id === productId;
+    });
+    if (productDetails) {
+      //updating the product
+      let newProduct = await body.value;
+      console.log(productDetails);
+      products = products.map((element) => {
+        return element.id === productId ? { ...newProduct } : element;
+      });
+      response.status = 200;
+      response.body = {
+        status: true,
+        data: newProduct,
+      };
+    } else {
+      response.status = 400;
+      response.body = {
+        status: false,
+        msg: "This product does not exist",
+      };
+    }
+  }
+);
+router.delete(
+  "/api/v1/products/:id",
+  async ({ params, response }: { params: { id: string }; response: any }) => {
+    //productId
+    let productId = params.id;
+    //get product details
+    let productDetails = products.find((element) => {
+      return element.id === productId;
+    });
+    if (productDetails) {
+      //get the index in array
+      let productIndex = products.indexOf(productDetails);
+      if (productIndex > -1) {
+        products.splice(productIndex, 1);
+      }
+      response.status = 200;
+      response.body = {
+        status: true,
+        data: productDetails,
+      };
+    } else {
+      response.status = 400;
+      response.body = {
+        status: false,
+        msg: "This product not exist",
+      };
+    }
+  }
+);
+export default router;

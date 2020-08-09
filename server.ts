c27ee03c722c1f1f9e170ead7f8d87a1c955ed6e
abdelfatah <a.abdelfatah@itv-me.com>
2020-08-10 03:33:04 +0400

    create-product model

--- server.ts
+++ server.ts
@@ -2,26 +2,6 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 const port = 5000;
-let products = [
-  {
-    id: "1",
-    title: "iphone 11",
-    price: 1000,
-    currency: "USD",
-  },
-  {
-    id: "2",
-    title: "iphone 12",
-    price: 2000,
-    currency: "USD",
-  },
-  {
-    id: "3",
-    title: "iphone 13",
-    price: 3000,
-    currency: "USD",
-  },
-];
 //get all products
 router.get("/api/v1/products", ({ response }: { response: any }) => {
   response.body = {
